564dc934302226d0306c1c1704cbe1898a8cf82e
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-07-09 17:58:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import discord
 from discord.ext import commands
 
 
-# Bot constructor, edit this to your needs.
+# bot constructor, edit this to your needs.
 bot = commands.Bot(command_prefix="!")
 
 
-#loading cogs, you can skip this part if you dont use cogs
+# loading cogs, you can skip this part if you dont use cogs
 [bot.load_extension(f"cogs.{cog[:-3]}") for cog in os.listdir("cogs") if cog.endswith(".py")]
 
-#basic command
+ #basic command
 @bot.command()
 async def command(ctx):
-    return await ctx.send("Hi") #sends the message `Hi` to the invoking channel.
+    await ctx.send("Hi") # sends the message `Hi` to the invoking channel.
 
 
 
-bot.run("token") #running a bot like this is not recommended but for the sake of this template, its gonna be like this
+bot.run("token") # running a bot like this is not recommended but for the sake of this template, its gonna be like this
