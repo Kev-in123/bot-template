fdcb872b9357b10658a3f5e75f6d8fda53398538
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-08-03 15:17:02 -0400

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -8,11 +8,11 @@ class Fun(commands.Cog): # basic cog
     
     @commands.command() # basic command
     async def ping(self, ctx):
-        return await ctx.send("pong!")
+        await ctx.send("pong!")
     
     @commands.command() # a command with arguments, invoked like !say <text>
     async def say(self, ctx, text):
-        return await ctx.send(text)
+        await ctx.send(text)
 
 def setup(bot): # this part is required for every cog
     bot.add_cog(Fun(bot))
