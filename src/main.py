616a84a9400a12641bdd70d115ae9a1f387914be
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-10-24 13:31:12 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,9 +9,7 @@ bot = commands.Bot(command_prefix="!")
 async def hi(ctx):
     await ctx.send("Hi") # sends the message `Hi` to the invoking channel.
 
-# loading all the cogs, you can skip this part if you dont use cogs or manually load all the cogs
-for cog in os.listdir("cogs"):
-  if cog.endswith(".py"):
-    bot.load_extension(f"cogs.{cog[:-3]}")
+    #loagind the cog
+bot.load_extension("cogs.fun")
 
 bot.run("token") # running a bot like this is not recommended (token is visible in the source) but for the sake of this template, it's gonna be like this
