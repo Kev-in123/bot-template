2597256a2cfdf6e52ffd38d6b3a6418c5796c604
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-10-24 13:31:27 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import os
 from discord.ext import commands
 
 # bot constructor, edit this to your needs.
@@ -9,7 +8,7 @@ bot = commands.Bot(command_prefix="!")
 async def hi(ctx):
     await ctx.send("Hi") # sends the message `Hi` to the invoking channel.
 
-    #loagind the cog
+#loading the cog
 bot.load_extension("cogs.fun")
 
 bot.run("token") # running a bot like this is not recommended (token is visible in the source) but for the sake of this template, it's gonna be like this
